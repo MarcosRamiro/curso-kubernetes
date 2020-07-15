875dda6243976b01ffcae9359ad83994aee792e7
Marcos Ramiro <mrsantos2006@hotmail.com>
2020-07-15 20:06:34 +0000

    Finalização do curso de Kubernetes

---
+++ sistema/enviarMensagem.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Olá, um novo pod acabou de ser criado!"}' https://hooks.slack.com/services/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
